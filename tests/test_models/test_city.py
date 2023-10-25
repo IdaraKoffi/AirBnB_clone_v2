cead6128f32f45069f3a7995028cffc09ba5ea1a
Fatimazohra95 <fatimazohra32@gmail.com>
2023-10-25 05:41:10 -0700

    Airbnb

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,90 +1,30 @@
 #!/usr/bin/python3
-"""test city"""
-import unittest
+"""Test city """
 import os
 from models.city import City
-from models.state import State
-from models.base_model import BaseModel
-import pep8
-from sqlalchemy import Column
-from os import getenv
+from tests.test_models.test_base_model import TestBasemodel
 
 
-class TestCity(unittest.TestCase):
-    """this will test the city class"""
+class TestCity(TestBasemodel):
+    """Represents the tests for the City model."""
+    def __init__(self, *args, **kwargs):
+        """Initializes the test class."""
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
 
-    @classmethod
-    def setUpClass(cls):
-        """set up for test"""
-        cls.city = City()
-        cls.city.name = "LA"
-        cls.state = State()
-        cls.state.name = "CA"
-        cls.city.state_id = cls.state.id
+    def test_state_id(self):
+        """Tests the type of state_id."""
+        new = self.value()
+        self.assertEqual(
+            type(new.state_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.city
-
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_City(self):
-        """test pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/city.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_checking_for_docstring_City(self):
-        """checking docstrings"""
-        self.assertIsNotNone(City.__doc__)
-
-    def test_attributes_City(self):
-        """chekcing  city have attributes"""
-        self.assertTrue('id' in self.city.__dict__)
-        self.assertTrue('created_at' in self.city.__dict__)
-        self.assertTrue('updated_at' in self.city.__dict__)
-        self.assertTrue('state_id' in self.city.__dict__)
-        self.assertTrue('name' in self.city.__dict__)
-
-    def test_is_subclass_City(self):
-        """test if city is subclass """
-        self.assertTrue(issubclass(self.city.__class__, BaseModel), True)
-
-    def test_attribute_types_City(self):
-        """test The attribute type for City"""
-        self.assertEqual(type(self.city.name), str)
-        self.assertEqual(type(self.city.state_id), str)
-
-    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db",
-                     "can't run if storage is db")
-    def test_save_City(self):
-        """test the save works"""
-        city = City()
-        city.name = 'LA'
-        state = State()
-        state.name = 'CA'
-        city.state_id = state.id
-        city.save()
-        self.assertNotEqual(city.created_at, city.updated_at)
-
-    def test_to_dict_City(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.city), True)
-
-#    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db",
-#    "can't run if storage is file")
-#    def test_attributes_v2_City(self):
-#        """Test the attributes in v2"""
-#        self.assertTrue(self.city.__tablename__ == "cities")
-#        self.assertTrue(type(self.city.name) == Column)
-#        self.assertTrue(type(self.city.state_id) == Column)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_name(self):
+        """Tests the type of name."""
+        new = self.value()
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
